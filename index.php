d7bb73cb7e068f7d7cdf142cc1e3db5d7a89f123
Diyar Parwana <diypa571@gmail.com>
2022-05-14 16:02:25 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,48 @@
 <?php
+session_start();
+// Kontrollera om session isset().
+$_SESSION['cart'] = $_SESSION['cart'] ?? [];
+
+
 include("./backend/config.php"); // Klassen inkluderas
 $objProducts = new Products(); // Ett objekt skapas
 $inStock = $objProducts->getData()['inStock'];
- ?>
- 
 
-<?php foreach ($inStock as $key => $product): ?>
+
+if(isset($_POST['add']))
+{
+$objProducts->addCart();
+}
+
+
+?>
+
+
+Kundvagn <?php echo count($_SESSION['cart']); ?>
+
+<ul class="list-group list-group-flush">
+<?php foreach ($_SESSION['cart'] as $key => $amount) {?>
+   <li class="list-group-item">
+    Product name:<strong> <?php echo $inStock[$key]['articleId'] . "</strong> |  amount:<strong> " . $amount . "</strong>"; ?>
+<?php } ?>
+</li>
+</ul>
+<a href="checkout.php" class="btn btn-success rounded-pill my-3">Till checkout</a>
+
+
+
+<hr>
+
+
+
+<?php foreach ($inStock as $key => $product){ ?>
 <?php echo " Product ID " .  $product['articleId'] . " " . "| " . " Quantity " . $product['quantity'] .  " Price " . $product['price'] ?> <br>
-<?php endforeach; ?>
+<form method="post" action="">
+<input type="hidden" name="productIndex" value="<?php echo $key ?>">
+<button class="btn btn-primary my-4 rounded-pill" name="add" type="submit">Lägg i kundvagn</button>
+</form>
+<hr>
+<?php } ?>
 
 <?php
 /*
