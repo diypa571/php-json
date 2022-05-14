d19e39fc3c0d7ee173db0b0d396efdd83f131e06
Diyar Parwana <diypa571@gmail.com>
2022-05-14 12:30:04 +0200

    config filen

--- backend/config.php
+++ backend/config.php
@@ -1,1 +1,21 @@
 
+<?php
+// Skapar en konstant för data json
+define("DATA_SOURCE", "demo_data.json");
+
+// Skapas en function som returnerar data som konverteras till PHP array
+function getData() {
+    // Kontrollera om filen existerar
+    if (!file_exists(__DIR__ . "/" . DATA_SOURCE)) {
+      // Filen existerar inte, skapa och skriva i filen
+      $dataInit = [
+        'inStock' => [],
+      ];
+
+      //  Funktionen file_put_contents för att skriva
+    file_put_contents(__DIR__ . "/" . DATA_SOURCE, json_encode($dataInit, JSON_PRETTY_PRINT));
+    }
+  
+	// Här returneras  data i json format
+	return json_decode(file_get_contents(__DIR__ . "/" . DATA_SOURCE), true);
+}
