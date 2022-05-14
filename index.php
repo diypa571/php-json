23c823211645e79bcf9766e51e683077503d32a7
Diyar Parwana <diypa571@gmail.com>
2022-05-14 12:32:38 +0200

    Update index.php

--- index.php
+++ index.php
@@ -7,3 +7,14 @@ $inStock = getData()['inStock'];
 <?php foreach ($inStock as $key => $product): ?>
 <?php echo " Product ID " .  $product['articleId'] . " " . "| " . " Quantity " . $product['quantity'] .  " Price " . $product['price'] ?> <br />
 <?php endforeach; ?>
+
+
+<?php
+/*
+Resultatet blir på följande sätt...
+Product ID 1 | Quantity 4 Price 12
+Product ID 2 | Quantity 2 Price 20
+Product ID 3 | Quantity 0 Price 100
+*/
+ 
+ ?>
